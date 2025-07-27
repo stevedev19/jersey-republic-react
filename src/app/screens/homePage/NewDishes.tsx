f67d943f32a5b48dcff036ec3ad28576d34f43fc
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-07-28 03:49:57 +0900

    fix: modify popular and new dishes components

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -8,57 +8,58 @@ import { CssVarsProvider } from "@mui/joy/styles";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import Divider from "../../components/divider";
 
-
 const newDishes = [
   { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-  { productName: "Kebab", imagePath: "/img/kebab.webp" },
   { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
   { productName: "Lavash", imagePath: "/img/lavash.webp" },
 ];
 
-export default function NewDishes () {
-    return (
-    <div className={"new-products-frame"}>
-        <Container>
-            <Stack className={"main"}>
-                <Box className={"category-title"}>Fresh Menu</Box>
-                <Stack className={"cards-frame"}>
-                    <CssVarsProvider>
-                        {newDishes.map((ele, index) => {
-                        return (
-                            <Card key={index} variant="outlined" className={"card"}>
-                             <CardOverflow>
-                                <div className="product-sale">Normal size</div>
-                                <AspectRatio ratio="1">
-                                    <img src={ele.imagePath} alt="" />
-                                </AspectRatio>
-                             </CardOverflow>
-                             <CardOverflow variant="soft" className="product-detail">
-                                <Stack className="info">
-                                    <Stack flexDirection={"row"}>
-                                        <Typography className={"title"}>
-                                            {ele.productName}
-                                        </Typography>
-                                        <Divider width="2" height="24" bg="#d9d9d9" />
-                                        <Typography className={"price"}>$12</Typography>
-                                        </Stack>
-                                        <Stack>
-                                        <Typography className={"views"}>
-                                        20
-                                        <VisibilityIcon
-                                         sx={{ fontSize: 20, marginLeft: "5px" }}
-                                          />
-                                      </Typography>
-</Stack>
-</Stack>
-</CardOverflow>
-</Card>
-                        );
-                    })}
-                    </CssVarsProvider>
-                                    </Stack>
-                                </Stack> 
-        </Container>
+export default function NewDishes() {
+  return (
+    <div className="new-products-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Fresh Menu</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {newDishes.length !== 0 ? (
+                newDishes.map((ele, index) => (
+                  <Card key={index} variant="outlined" className="card">
+                    <CardOverflow>
+                      <div className="product-sale">Normal size</div>
+                      <AspectRatio ratio="1">
+                        <img src={ele.imagePath} alt={ele.productName} />
+                      </AspectRatio>
+                    </CardOverflow>
+                    <CardOverflow variant="soft" className="product-detail">
+                      <Stack className="info">
+                        <Stack flexDirection="row">
+                          <Typography className="title">
+                            {ele.productName}
+                          </Typography>
+                          <Divider width="2" height="24" bg="#d9d9d9" />
+                          <Typography className="price">$12</Typography>
+                        </Stack>
+                        <Stack>
+                          <Typography className="views">
+                            20
+                            <VisibilityIcon
+                              sx={{ fontSize: 20, marginLeft: "5px" }}
+                            />
+                          </Typography>
+                        </Stack>
+                      </Stack>
+                    </CardOverflow>
+                  </Card>
+                ))
+              ) : (
+                <Box className="no-data">New products are not available!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
-    ); 
+  );
 }
