e2a5e2023e1bde8ae5bcecfd89336e14471ffe96
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-11-25 12:15:31 +0900

    feat: develop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./css/index.css";
 import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
+import "./css/index.css";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -18,12 +19,14 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
+        <SocketProvider>
        <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <App />
       </Router>
       </ThemeProvider>
+      </SocketProvider>
     </ContextProvider>  
     </Provider>
   </React.StrictMode>,
